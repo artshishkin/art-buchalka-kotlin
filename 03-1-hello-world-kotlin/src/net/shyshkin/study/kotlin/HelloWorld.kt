321dfac9f001bc7753b9a60a3fdede24de21af6e
Art <d.art.shishkin@gmail.com>
2024-09-06 13:47:52 +0300

    18. Variable Declarations in Kotlin (#2)

--- 03-1-hello-world-kotlin/src/net/shyshkin/study/kotlin/HelloWorld.kt
+++ 03-1-hello-world-kotlin/src/net/shyshkin/study/kotlin/HelloWorld.kt
@@ -1,5 +1,12 @@
 package net.shyshkin.study.kotlin
 
+import net.shyshkin.study.kotlin.declarations.declarations
+
 fun main(args: Array<String>) {
     println("Hello, World!")
+
+    declarations()
+
 }
+
+class Employee(var name: String, val id: Int)
---
+++ 03-1-hello-world-kotlin/src/net/shyshkin/study/kotlin/declarations/Declarations.kt
@@ -0,0 +1,42 @@
+package net.shyshkin.study.kotlin.declarations
+
+import net.shyshkin.study.kotlin.Employee
+
+fun declarations() {
+    //val
+    val numberInferenceType = 25
+    val numberWithType: Int
+
+    numberWithType = 3
+    //numberWithType=5 // NOT ALLOWED
+
+    val numberWithBoth: Short = 23
+
+    //var
+    var numberReAssigned: Int
+
+    numberReAssigned = 3
+    numberReAssigned = 5
+
+    val employee1 = Employee("Art", 123)
+
+    //var - mutable variable
+    employee1.name = "Kate"
+
+    //val - immutable
+    //employee1.id = 100
+    //or
+    //employee1 = Employee("Arina", 321)
+
+    val employee2: Employee
+    val number = 100
+
+    //val could be assigned only once
+    if (number < 100) {
+        employee2 = Employee("Nazar", 234)
+    } else {
+        employee2 = Employee("Tanya", 333)
+    }
+
+}
+
