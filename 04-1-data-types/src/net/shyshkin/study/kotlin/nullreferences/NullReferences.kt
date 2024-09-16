33743f6c8101dda70de729e5923f476e9c32d691
Art <d.art.shishkin@gmail.com>
2024-09-16 21:14:48 +0300

    31. Null References in Kotlin - Part Two (#3)

--- 04-1-data-types/src/net/shyshkin/study/kotlin/nullreferences/NullReferences.kt
+++ 04-1-data-types/src/net/shyshkin/study/kotlin/nullreferences/NullReferences.kt
@@ -18,9 +18,41 @@ fun main(args: Array<String>) {
     // ?: - elvis operator
     println("Providing default value = ${str2?.lowercase()?.reversed()?.plus("<<<") ?: "this is the default value"}")
 
-    val something: Any = arrayOf(1,2,3,4)
+    val something: Any = arrayOf(1, 2, 3, 4)
     val str3 = something as? String //safe `CAST` operator
     println(str3)
     println(str3?.uppercase())
 
+    val str4 = str!!.uppercase() // Not-null assertion operator - in case you are sure `str` is NOT null or you want NPE exception
+
+    println(str4)
+
+    val strNull: String? = null
+
+    try {
+        val str5 = strNull!!  // WILL THROW NPE
+        val str6 = str5.uppercase()
+        println(str6)
+    } catch (ex: NullPointerException) {
+        ex.printStackTrace()
+    }
+
+    try {
+        printText(str!!)
+        printText(strNull!!) // WILL THROW NPE
+    } catch (ex: NullPointerException) {
+        ex.printStackTrace()
+    }
+
+    str?.let { printText(it) } // `let` function is called when str is not null
+    strNull?.let { printText(it) } // `let` function is called when strNull is not null
+
+    println(strNull == str)
+
+    println("end")
+
+}
+
+fun printText(text: String) {
+    println(text)
 }
