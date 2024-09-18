529619128aae39132a0823672e13d903c96dded7
Art <d.art.shishkin@gmail.com>
2024-09-18 13:00:47 +0300

    43. Inheritance in Kotlin - Part Two (#5)

--- 05-1-oop/src/net/shyshkin/study/kotlin/inheritance/Inheritance.kt
+++ 05-1-oop/src/net/shyshkin/study/kotlin/inheritance/Inheritance.kt
@@ -9,4 +9,15 @@ fun inheritance(){
 
     println("Printer type: ${laserPrinter.type()}, best price: ${laserPrinter.bestSellingPrice()}")
 
+    val hp = NotMatchingConstructorLaserPrinter("HP", "super laser", 15)
+    println("NotMatchingConstructorLaserPrinter type: ${hp.type()}, ppm: ${hp.ppm}")
+
+    val defaultLaserPrinter = DefaultLaserPrinter()
+    println("Default laser printer: ${defaultLaserPrinter.modelName}")
+
+    val my3DColor1 = My3DColor("green", "deep0.1")
+    println(my3DColor1)
+    val my3DColor2 = My3DColor("blue")
+    println(my3DColor2)
+
 }
--- 05-1-oop/src/net/shyshkin/study/kotlin/inheritance/Printer.kt
+++ 05-1-oop/src/net/shyshkin/study/kotlin/inheritance/Printer.kt
@@ -10,19 +10,74 @@ abstract class Printer(val modelName: String) {
 
     open fun printModel() = println("The model name of this printer is `$modelName`")
 
-    abstract fun type():String
+    abstract fun type(): String
 
     abstract fun bestSellingPrice(): BigDecimal
 }
 
-class LaserPrinter(modelName: String) : Printer(modelName) {
+open class LaserPrinter(modelName: String) : Printer(modelName) {
 
-    override fun printModel() = println("The model name of this Laser printer is `$modelName`")
+    //we want that every subclass of a LaserPrinter uses THIS implementation of a function
+    //so we disable this fun to be overriden by using `final` keyword
+    final override fun printModel() = println("The model name of this Laser printer is `$modelName`")
     override fun type() = "laser"
 
     override fun bestSellingPrice() = BigDecimal("123.56")
 
 }
+
+class SpecialLaserPrinter(modelName: String) : LaserPrinter(modelName) {
+
+    // NOT ALLOWED
+    // override fun printModel() = println("Special Laser Printer: $modelName")
+
+}
+
+class NotMatchingConstructorLaserPrinter(modelName: String, private val type: String, val ppm: Int = 8) : LaserPrinter(modelName) {
+    override fun type() = type
+}
+
+class DefaultLaserPrinter() : LaserPrinter("SomeDefaultPrinter") {
+}
+
+//class without primary constructor
+open class Color {
+
+    val colorName: String
+    val deepness: String
+
+    //secondary constructor 1
+    constructor(colorName: String, deepness: String) {
+        this.colorName = colorName
+        this.deepness = deepness
+        println("I'm in the parent's constructor")
+    }
+
+    //secondary constructor 2
+    constructor(colorName: String) {
+        this.colorName = colorName
+        this.deepness = "too deep"
+    }
+
+    override fun toString(): String {
+        return "Color(colorName='$colorName', deepness='$deepness')"
+    }
+
+}
+
+// if there is a primary constructor then every constructor in the child should use parent primary constructor
+class My3DColor : Color {
+
+    //secondary constructor 1
+    constructor(colorName: String, deepness: String) : super(colorName, deepness){
+        println("I'm in the child's constructor")
+    }
+
+    //secondary constructor 2
+    constructor(colorName: String) : super(colorName)
+
+}
+
 
 //open class Printer(val modelName: String) {
 //}
@@ -33,3 +88,7 @@ class LaserPrinter(modelName: String) : Printer(modelName) {
 //class LaserPrinter : Printer {
 //    constructor(): super()
 //}
+
+// DATA CLASS CAN NOT BE EXTENDED
+//open data class SomeDataClass(val someNum:Int) //Modifier 'data' is incompatible with 'open'
+//abstract data class SomeDataClass(val someNum:Int) //Modifier 'abstract' is incompatible with 'data'
