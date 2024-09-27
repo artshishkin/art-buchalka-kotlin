c28f973f5889abf86ba8e7bb1505848b4f08c913
Art <d.art.shishkin@gmail.com>
2024-09-27 10:17:46 +0300

    45. Singletons in Kotlin (#5)

---
+++ 05-1-oop/src/net/shyshkin/study/kotlin/singleton/CompanyCommunications.kt
@@ -0,0 +1,16 @@
+package net.shyshkin.study.kotlin.singleton
+
+import java.time.Year
+
+fun main(args: Array<String>) {
+    println(CompanyCommunications.getTagLine())
+    println(CompanyCommunications.getCopyrightLine())
+}
+
+object CompanyCommunications {
+
+    val currentYear = Year.now().value
+
+    fun getTagLine() = "Our company rocks!"
+    fun getCopyrightLine() = "Copyright \u00A9 $currentYear Our Company. All rights reserved."
+}
