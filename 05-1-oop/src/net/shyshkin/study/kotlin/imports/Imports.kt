02b99c1d28f900de5f25e45ef1cec8936bf13462
Art <d.art.shishkin@gmail.com>
2024-10-04 19:24:37 +0300

    49.4 Imports in Kotlin - Importing top-level object and enums (#5)

--- 05-1-oop/src/net/shyshkin/study/kotlin/imports/Imports.kt
+++ 05-1-oop/src/net/shyshkin/study/kotlin/imports/Imports.kt
@@ -1,7 +1,14 @@
 package net.shyshkin.study.kotlin.imports
 
+import net.shyshkin.study.kotlin.enums.DepartmentEnhanced.IT
 import net.shyshkin.study.kotlin.imports.another.topLevelFunction
+import net.shyshkin.study.kotlin.singleton.CompanyCommunications
 
 fun main(args: Array<String>) {
     topLevelFunction("Hi from other")
+
+    println(CompanyCommunications.getCopyrightLine())
+
+    println(IT.getDepartmentInfo())
+
 }
